7140d60917c5da928ff9d413515ef97d57639265
thunder <119463825+Thunderrock424242@users.noreply.github.com>
2026-04-12 21:32:51 -0400

    Fix wall wire rendering to look like redstone

--- src/main/java/com/thunder/stickyredstone/core/stickyredstone.java
+++ src/main/java/com/thunder/stickyredstone/core/stickyredstone.java
@@ -3,8 +3,11 @@ package com.thunder.stickyredstone.core;
 import com.mojang.logging.LogUtils;
 import com.thunder.stickyredstone.block.ModBlocks;
 import com.thunder.stickyredstone.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
 
 @Mod(stickyredstone.MOD_ID)
@@ -19,6 +22,15 @@ public class stickyredstone {
         ModBlocks.BLOCKS.register(modEventBus);
         ModItems.ITEMS.register(modEventBus);
 
+        modEventBus.addListener(stickyredstone::onClientSetup);
+
         LOGGER.info("stickyredstone loaded!");
+    }
+
+    private static void onClientSetup(final FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            ItemBlockRenderTypes.setRenderLayer(ModBlocks.WALL_REDSTONE_WIRE.get(), RenderType.cutout());
+            ItemBlockRenderTypes.setRenderLayer(ModBlocks.STICKY_REDSTONE_WIRE.get(), RenderType.cutout());
+        });
     }
 }
