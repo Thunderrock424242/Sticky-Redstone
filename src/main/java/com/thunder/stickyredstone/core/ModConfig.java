7dfba9bd119999acf5aa8924579dcbbc7b22fc45
Thunderrock424242 <masonjoshua8@gmail.com>
2026-04-14 21:20:36 -0400

    fixed issue

--- src/main/java/com/thunder/stickyredstone/core/ModConfig.java
+++ src/main/java/com/thunder/stickyredstone/core/ModConfig.java
@@ -6,10 +6,6 @@ public class ModConfig {
 
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    public static final ModConfigSpec.BooleanValue VANILLA_DUST_WALLS_AND_CEILINGS = BUILDER
-            .comment("When true, vanilla redstone dust places sticky wire on floors/walls/ceilings. When false, use the mod's sticky wire item.")
-            .define("vanillaDustWallsAndCeilings", false);
-
     public static final ModConfigSpec SPEC = BUILDER.build();
 
     private ModConfig() {
